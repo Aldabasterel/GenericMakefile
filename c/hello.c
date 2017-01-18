7cc850643a84cbbfe00b95254cda6a93b9de7435
denischevalier <chevalierdenis@gmx.com>
2017-01-19 00:01:28 +0100

    Changing spaces for tabs

--- c/hello.c
+++ c/hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-  // suppress warnings
-  (void)argc;
-  (void)argv;
+	// suppress warnings
+	(void)argc;
+	(void)argv;
 
-  printf("Hello World!\n");
-  return 0;
+	printf("Hello World!\n");
+	return 0;
 }
